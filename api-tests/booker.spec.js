518fcabfb520b8b30e970df460d97bab158ce8fd
colin-c6 <colincaraher@gmail.com>
2023-04-24 20:58:53 +0100

    test: Added Test to Post a new booking

--- api-tests/booker.spec.js
+++ api-tests/booker.spec.js
@@ -3,5 +3,37 @@ import { test, expect } from "@playwright/test";
 test("GET all bookings", async ({ request }) => {
   const response = await request.get(`/booking`);
   expect(response.status()).toBe(200);
-  console.log(await response.json());
+});
+
+test("POST a booking", async ({ request }) => {
+  const response = await request.post(`/booking`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    data: {
+      firstname: "Stephen",
+      lastname: "Kenny",
+      totalprice: 999,
+      depositpaid: false,
+      bookingdates: {
+        checkin: "2023-01-01",
+        checkout: "2023-04-01",
+      },
+      additionalneeds: "Football",
+    },
+  });
+  // Assert Status
+  expect(response.status()).toBe(200);
+
+  const res = await response.json();
+  // Assert Response Property Types
+  expect(typeof res.bookingid).toBe("number");
+  expect(typeof res.booking).toBe("object");
+
+  // Assert Important Response Property Values
+  expect(res.booking.firstname).toBe("Stephen");
+  expect(res.booking.lastname).toBe("Kenny");
+  expect(res.booking.totalprice).toBe(999);
+  expect(res.booking.depositpaid).toBe(false);
 });
